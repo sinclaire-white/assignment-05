15f41dbf7fc11d761845eca907d9e944a425d8f4
sinclaire-white <shahriyarsifat2@gmail.com>
2025-03-01 15:32:26 +0600

    basic HTML structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
